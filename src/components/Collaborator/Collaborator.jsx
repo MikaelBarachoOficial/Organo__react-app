2ccfbb751e789426898b9962e19620379903bec8
Mikael <kaelbaracho@gmail.com>
2023-09-20 21:51:07 -0300

    Form and Teams dynamic

--- src/components/Collaborator/Collaborator.jsx
+++ src/components/Collaborator/Collaborator.jsx
@@ -5,11 +5,11 @@ export const Collaborator = (props) => {
     return (
         <div className='collaborator-card'>
             <div className='collaborator-card__top' style={{backgroundColor: props.teamColor}}>
-                <img src={props.image} alt={props.name} />
+                <img src={props.collaborator.image} alt={props.collaborator.name} />
             </div>
             <div className='collaborator-card__bottom'>
-                <h4>{props.name}</h4>
-                <h5>{props.job}</h5>
+                <h4>{props.collaborator.name}</h4>
+                <h5>{props.collaborator.job}</h5>
             </div>
         </div>
     )
--- src/components/Teams/Team.jsx
+++ src/components/Teams/Team.jsx
@@ -1,17 +1,16 @@
 import { Collaborator } from '../Collaborator/Collaborator.jsx'
 import './Team.css'
 
-export const Team = (props) => {
+export const Team = ({team, collaborators}) => {
     return (
-        <section className='team' style={{backgroundColor: props.team.backgroundColor}}>
+        <section className='team' style={{backgroundColor: team.backgroundColor}}>
             <div className='team__title'>
-                <h3>{props.team.name}</h3>
-                <hr style={{borderColor: props.team.mainColor}}/>
+                <h3>{team.name}</h3>
+                <hr style={{borderColor: team.mainColor}}/>
             </div>
 
             <div className='team__collaborators'>
-                <Collaborator name='Mikael Baracho' job='Programador, Front-End, React.js' image='https://github.com/MikaelBarachoOficial.png' teamColor={props.team.mainColor} />
-                <Collaborator name='Andressa Sales' job='Programadora HTML, Copiadora de código' image='https://github.com/AndressaSales.png' teamColor={props.team.mainColor} />
+                {collaborators.map((collaborator, index) => <Collaborator key={index} collaborator={collaborator} teamColor={team.mainColor} />)}
             </div>
 
         </section>
